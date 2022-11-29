00984e3ba0529e8151996f9646738c00013d6f34
Bitcoinera <anagjordano@gmail.com>
2022-11-29 11:29:25 +0100

    add second prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,8 +6,11 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix =
-  "Write me a blog post in the style of Paul Graham with the title below. Please make sure the blog post goes in-depth on the topic and shows that the writer did their research.\nTitle: ";
+const basePromptPrefix = `
+Write me a detailed table of contents for a blog post with the title below.
+
+Title:
+`;
 
 const generateAction = async (req, res) => {
   // Run first prompt
@@ -16,13 +19,37 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 500,
+    temperature: 0.8,
+    max_tokens: 250,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  // build second prompt
+  const secondPrompt = `
+  Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+
+  Title: ${req.body.userInput}
+
+  Table of Contents: ${basePromptOutput.text}
+
+  Blog Post:
+  `;
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${secondPrompt}`,
+    // set a higher temperature for this one
+    temperature: 0.85,
+    // also increase max_tokens.
+    max_tokens: 1250,
+  });
+
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
